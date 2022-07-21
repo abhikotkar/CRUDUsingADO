754f64314ff6e015e8198ce67bc17ed628748ab7
Abhijeet <Abhijeet@DESKTOP-LQC2102>
2022-07-21 12:48:47 +0530

    Add project files.

---
+++ CRUDUsingADO/DAL/EmployeeDAL.cs
@@ -0,0 +1,98 @@
+﻿using CRUDUsingADO.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDUsingADO.DAL
+{
+    public class EmployeeDAL
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+        public EmployeeDAL()
+        {
+            con = new SqlConnection(Startup.ConnectionString);
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> elist = new List<Employee>();
+            string qry = "select * from Employee";
+            cmd = new SqlCommand(qry, con);
+            con.Open();
+            dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    Employee e = new Employee();
+                    e.ID = Convert.ToInt32(dr["ID"]);
+                    e.Name = dr["Name"].ToString();
+                    e.Salary = Convert.ToDouble(dr["Salary"]);
+                    elist.Add(e);
+                }
+            }
+            con.Close();
+            return elist;
+        }
+        public Employee GetEmployeeById(int id)
+        {
+            Employee e = new Employee();
+            string qry = "select * from Employee where ID=@id";
+            cmd = new SqlCommand(qry, con);
+            cmd.Parameters.AddWithValue("@id", id);
+            con.Open();
+            dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    e.ID= Convert.ToInt32(dr["ID"]);
+                    e.Name = dr["Name"].ToString();
+                    e.Salary = Convert.ToDouble(dr["Salary"]);
+                }
+            }
+            con.Close();
+            return e;
+        }
+
+        public int AddEmployee(Employee emp)
+        {
+            string qry = "insert into Employee values(@name,@salary)";
+            cmd = new SqlCommand(qry, con);
+
+            cmd.Parameters.AddWithValue("@name", emp.Name);
+            cmd.Parameters.AddWithValue("@salary", emp.Salary);
+            con.Open();
+            int result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+        public int UpdateEmployee(Employee emp)
+        {
+            string qry = "update Employee set Name=@name , Salary=@salary where ID=@id";
+            cmd = new SqlCommand(qry, con);
+            cmd.Parameters.AddWithValue("@id", emp.ID);
+            cmd.Parameters.AddWithValue("@name", emp.Name);
+            cmd.Parameters.AddWithValue("@salary", emp.Salary);
+            con.Open();
+            int result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+        public int DeleteEmployee(int id)
+        {
+            string qry = "delete from Employee where ID=@id";
+            cmd = new SqlCommand(qry, con);
+            cmd.Parameters.AddWithValue("@id", id);
+            con.Open();
+            int result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+    }
+}
---
+++ CRUDUsingADO/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDUsingADO.Models
+{
+    [Table("Product")]
+    public class Product  //model class or POCO class(Plain Old C#/CLP Object) or BO (Bizness Object class)
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Product name is required")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Product Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        [Display(Name = "Product Price")]
+        [Range(minimum: 1, maximum: 50000)]
+        public double Price { get; set; }
+    }
+}
